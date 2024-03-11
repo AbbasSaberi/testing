502cf5ea6edd9334fc9addcbe154d6afbfeec27e
Hossein2468 <hosseinbaziepic@gmail.com>
2024-03-11 10:52:27 +0330

    Added p016_ui

---
+++ p016_ui.py
@@ -0,0 +1,35 @@
+from nicegui import ui
+from p016_solution import solution
+
+class Options:
+    def __init__(self):
+        self.visible = False 
+        self.first = True
+
+options = Options()
+
+def show_ui(visible):
+    options.visible = visible
+    if options.first:
+        options.first = False
+        root = create_ui()
+    
+def create_ui():
+    with ui.column().bind_visibility_from(options, 'visible') as root:
+        numberInput = ui.input('Number')
+        ui.button('Check Number', on_click=lambda: check_number())
+        output = ui.label()
+
+    def check_number():
+        number = int(numberInput.value)
+        textatea = ui.textarea(label='Result')
+        textatea.value = '\n'.join(number)
+        textatea.enable = False
+        result = solution(number)
+        textatea.value = result
+
+    return root
+
+if __name__ in {"__main__", "__mp_main__"}:
+    show_ui(True)
+    ui.run()
